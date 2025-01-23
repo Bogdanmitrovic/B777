8762e377a20f2e849e781f296e6d6a86b39e48c2
39matt <matija.todosijevic.i4@gmail.com>
2025-01-23 15:10:47 +0100

    Initialized checklist

--- Assets/Resources/Scripts/ChecklistRenderer.cs
+++ Assets/Resources/Scripts/ChecklistRenderer.cs
@@ -1,14 +1,19 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChecklistRenderer : MonoBehaviour
 {
+    private VerticalLayoutGroup _verticalLayoutGroup;
     public int characterCount = 50;
     public int splitNameLimit = 20;
     public GameObject checkListParent;
     public GameObject checkPrefabAuto;
     public GameObject checkPrefabManual;
     public TMP_Text checksTMP;
+    public GameObject checkInstaceManual;
+    public GameObject checkInstaceAuto;
     void Start()
     {
         Checklist checklist = new Checklist();
@@ -17,7 +22,16 @@ public class ChecklistRenderer : MonoBehaviour
         checklist.Checks.Add(new Check { Name = "Checkcheckcheckcheckcheck 3", ExpectedValue = "Expected 3 AAAAA aa :)" });
         var checkTexts = checklist.CheckTexts(characterCount, splitNameLimit);
         string allText = string.Join("\n", checkTexts);
-        checksTMP.text = allText;
+        _verticalLayoutGroup = checkListParent.GetComponent<VerticalLayoutGroup>();
+        
+        foreach (var check in checklist.Checks)
+        {
+            checkInstaceManual = Instantiate(checkPrefabManual);
+            checkInstaceManual.GetComponentInChildren<TMP_Text>().text = check.Text(characterCount, splitNameLimit);
+            checkInstaceManual.transform.SetParent(checkListParent.transform, false);
+            checkInstaceManual.GetComponentInChildren<TMP_Text>().rectTransform.offsetMin = new Vector2(-700, checkInstaceManual.GetComponentInChildren<TMP_Text>().rectTransform.offsetMin.y);
+            checkInstaceManual.GetComponentInChildren<TMP_Text>().rectTransform.offsetMax = new Vector2(700, checkInstaceManual.GetComponentInChildren<TMP_Text>().rectTransform.offsetMax.y);
+        }
     }
 
     void Update()
