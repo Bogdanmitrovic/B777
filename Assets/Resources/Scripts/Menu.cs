65371b540c98481908803df977648b0312a662d0
39matt <matija.todosijevic.i4@gmail.com>
2025-01-28 21:36:05 +0100

    added menu classes to read from json

---
+++ Assets/Resources/Scripts/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class MenuItem
+{
+    public string MenuName;
+    public List<ListItem> Lists;
+}
+
+[Serializable]
+public class ListItem
+{
+    public string ListName;
+    public List<ChecklistItem> List;
+}
+
+[Serializable]
+public class ChecklistItem
+{
+    public string name;
+    public string expectedValue;
+    public List<ValueOption> value; // For nested "value" field
+}
+
+[Serializable]
+public class ValueOption
+{
+    public Dictionary<string, string> Yes;
+    public Dictionary<string, string> No;
+}
+[Serializable]
+public class Wrapper
+{
+    public List<MenuItem> Menus;
+}
